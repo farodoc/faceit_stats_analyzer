bf026484876aeb42c49a6b22d4f340752e436ff5
Jakub Konopka <panjakubkonopka@gmail.com>
2023-08-22 16:28:36 +0200

    added table

--- faceit_stat_analyzer/Controllers/HomeController.cs
+++ faceit_stat_analyzer/Controllers/HomeController.cs
@@ -18,16 +18,30 @@ namespace faceit_stat_analyzer.Controllers
             return View();
         }
 
+
+
         public IActionResult TestPage()
         {
-            List<TestViewModel> list = new List<TestViewModel>();
-            for (int i = 0; i < 10; i++)
+            List<GameViewModel> list = new List<GameViewModel>();
+            for (int i = 0; i < 20; i++)
             {
-                list.Add(new TestViewModel
+                list.Add(new GameViewModel
                 {
-                    Name = "Name " + i,
-                    Description = "Description " + i
-                });
+                    Date = new DateTime(2023, 8, 22),
+                    Score = "16:0",
+                    FirstHalfScore = "15:0",
+                    SecondHalfScore = "1:0",
+                    EloAfter = 3000,
+                    EloGain = +25,
+                    Map = "de_mirage",
+                    Team = "team_--faro--",
+                    Stats = "31-5-6",
+                    Kd = 5.16,
+                    Kr = 1.93,
+                    Hs = 63,
+                    HLTV = 2.31
+
+                }); ;
             }
 
             return View(list);
--- faceit_stat_analyzer/Models/TestViewModel.cs
+++ faceit_stat_analyzer/Models/TestViewModel.cs
@@ -1,8 +1,21 @@
-﻿namespace faceit_stat_analyzer.Models
+﻿using System.Drawing;
+
+namespace faceit_stat_analyzer.Models
 {
-    public class TestViewModel
+    public class GameViewModel
     {
-        public string Name;
-        public string Description;
+        public DateTime Date;
+        public string Score;
+        public string FirstHalfScore;
+        public string SecondHalfScore;
+        public int EloAfter;
+        public int EloGain;
+        public string Map;
+        public string Team;
+        public string Stats;
+        public double Kd;
+        public double Kr;
+        public int Hs;
+        public double HLTV;
     }
 }
